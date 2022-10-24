9b186087158dc458f9312a6c2a9d70295f7dc2d3
Hbarni <hajtmanszkib@gmail.com>
2022-10-24 18:30:51 +0200

    cleanup bc i forgot :)

--- src/modpacks.rs
+++ src/modpacks.rs
@@ -1,14 +1,11 @@
-use  {
+use {
+    crate::end,
+    dialoguer::{theme::ColorfulTheme, Confirm, Select},
     std::fs,
-    dialoguer::{theme::ColorfulTheme, Select, Confirm},
-    crate::end
 };
 
 pub async fn pick_modpack() -> &'static str {
-    let modpacks = &[
-        "1.18.2 fabric modpack",
-        "1.19.2 preformance modpack",
-    ];
+    let modpacks = &["1.18.2 fabric modpack", "1.19.2 preformance modpack"];
 
     let picked_modpack = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Pick modpack to install:")
@@ -17,40 +14,42 @@ pub async fn pick_modpack() -> &'static str {
         .interact()
         .unwrap();
 
-    let downloadurl = 
-    if modpacks[picked_modpack] == "1.18.2 fabric modpack" {"https://drive.google.com/uc?export=download&id=1qa7gThngkqNooUweuyVs6Kes8w_pIJ0l&confirm=t"} 
-    else if modpacks[picked_modpack] == "1.19.2 preformance modpack" {"https://drive.google.com/uc?export=download&id=1ga7ySowdBsR0QxxEyQbEaKWF95JhERXe&confirm=t"} 
-    else {""};
+    let downloadurl = if modpacks[picked_modpack] == "1.18.2 fabric modpack" {
+        "https://drive.google.com/uc?export=download&id=1qa7gThngkqNooUweuyVs6Kes8w_pIJ0l&confirm=t"
+    } else if modpacks[picked_modpack] == "1.19.2 preformance modpack" {
+        "https://drive.google.com/uc?export=download&id=1ga7ySowdBsR0QxxEyQbEaKWF95JhERXe&confirm=t"
+    } else {
+        ""
+    };
 
     downloadurl
 }
 
 pub fn helpmsg() {
-        if Confirm::with_theme(&ColorfulTheme::default())
-            .with_prompt("Show install help?")
-            .interact()
-            .unwrap()
-        {
-            println!("\nInstall help:");
-            println!("\nCreate a new installation in your minecraft launcher\nwith the path of where you installed the modpack");
-            println!("Then find the version corresponding to what modpack you installed\nfor example fabric 1.18.2 will need \nsomething similar to: \"fabric-loader-0.14.9-1.18.2\"");
-            println!("Your loader version has alredy been installed to your minecraft launcher");
-            println!("\nAdditionally it is recommended to give the game between 4-10gb of ram in the advanced options");
+    if Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Show install help?")
+        .interact()
+        .unwrap()
+    {
+        println!("\nInstall help:");
+        println!("\nCreate a new installation in your minecraft launcher\nwith the path of where you installed the modpack");
+        println!("Then find the version corresponding to what modpack you installed\nfor example fabric 1.18.2 will need \nsomething similar to: \"fabric-loader-0.14.9-1.18.2\"");
+        println!("Your loader version has alredy been installed to your minecraft launcher");
+        println!("\nAdditionally it is recommended to give the game between 4-10gb of ram in the advanced options");
+    }
 }
-}
-
 
 pub fn tryagain(mods_dir: &str) {
     if Confirm::with_theme(&ColorfulTheme::default())
-            .with_prompt("Try again?")
-            .interact()
-            .unwrap()
-        {
-            fs::remove_dir_all(format!("{}mods", mods_dir)).unwrap_or_else(|error| {
-                println!("Failed again here is your error:\n{}", error);
-                end();
-            })
-        } else {
-            std::process::exit(0)
-        }
+        .with_prompt("Try again?")
+        .interact()
+        .unwrap()
+    {
+        fs::remove_dir_all(format!("{}mods", mods_dir)).unwrap_or_else(|error| {
+            println!("Failed again here is your error:\n{}", error);
+            end();
+        })
+    } else {
+        std::process::exit(0)
     }
+}
