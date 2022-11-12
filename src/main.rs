a11b06be374c4079a5632a93ccbe61061ce6658d
Hbarni <hajtmanszkib@gmail.com>
2022-11-12 11:51:19 +0100

    added support for in game update button

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,54 @@
 mod modpacks;
 use {
-    std::{io, io::Write, fs, fs::File, path::{Path, PathBuf}, cmp::min},
-    reqwest::Client,
-    indicatif::{ProgressBar, ProgressStyle},
-    futures_util::StreamExt,
-    zip_extensions::*,
     directories::BaseDirs,
-    fs_extra::dir::{move_dir, CopyOptions}
+    fs_extra::dir::{move_dir, CopyOptions},
+    futures_util::StreamExt,
+    indicatif::{ProgressBar, ProgressStyle},
+    reqwest::Client,
+    std::{
+        cmp::min,
+        env, fs,
+        fs::File,
+        io,
+        io::Write,
+        path::{Path, PathBuf},
+    },
+    zip_extensions::*,
 };
 
 #[tokio::main]
 async fn main() {
-    let downloadurl = modpacks::pick_modpack().await;
+    let args: Vec<String> = env::args().collect();
+    let downloadurl = if args.get(1) == None {
+        modpacks::pick_modpack().await
+    } else if args[1] == "gameupdate" {
+        "https://drive.google.com/uc?export=download&id=1qa7gThngkqNooUweuyVs6Kes8w_pIJ0l&confirm=t"
+    } else {
+        println!("Argument not found\nInvalid argument: {}", args[1]); end(); ""
+    };
     let update: bool = Path::new("./mods").exists();
-    let mcmodszip = if update == false {"./modpack/mcmods.zip"} else {"./mcmods.zip"};
-    let mods_dir = if update == false {"./modpack/"} else {"./"};
-    if update == false {fs::create_dir(&mods_dir).ok();}
-    else {
+    let mcmodszip = if update == false {
+        "./modpack/mcmods.zip"
+    } else {
+        "./mcmods.zip"
+    };
+    let mods_dir = if update == false { "./modpack/" } else { "./" };
+    if update == false {
+        fs::create_dir(&mods_dir).ok();
+    } else {
         println!("Found existing modpack updating...");
         fs::remove_dir_all(format!("{}mods", mods_dir)).unwrap_or_else(|_e| {
             println!("Unable to delete old mods this might be because you are running the game.");
             modpacks::tryagain(mods_dir)
-
         });
         fs::remove_dir_all(format!("{}versions", mods_dir)).ok();
 
         println!("Deleted old mods and installations")
     };
-    
-    download_file(&Client::new(), downloadurl, mcmodszip).await.unwrap();
+
+    download_file(&Client::new(), downloadurl, mcmodszip)
+        .await
+        .unwrap();
 
     println!("\nExtracting archive...");
     let mczip = PathBuf::from(&mcmodszip);
@@ -39,7 +59,12 @@ async fn main() {
     if let Some(base_dirs) = BaseDirs::new() {
         let appdata = base_dirs.data_dir().to_str().expect("An error occured");
         let options = CopyOptions::new();
-        move_dir(format!("{}versions", &mods_dir), format!(r"{}\.minecraft\", &appdata), &options).ok();
+        move_dir(
+            format!("{}versions", &mods_dir),
+            format!(r"{}\.minecraft\", &appdata),
+            &options,
+        )
+        .ok();
         println!(r"Sent versions to {}\.minecraft\versions", &appdata)
     };
 
@@ -47,8 +72,10 @@ async fn main() {
     fs::remove_file(&mcmodszip).ok();
     fs::remove_dir_all(format!("{}versions", mods_dir)).ok();
 
-    if update == false {modpacks::helpmsg();}
-        
+    if update == false {
+        modpacks::helpmsg();
+    }
+
     end()
 }
 async fn download_file(client: &Client, url: &str, path: &str) -> Result<(), String> {
@@ -58,34 +85,34 @@ async fn download_file(client: &Client, url: &str, path: &str) -> Result<(), Str
         .send()
         .await
         .or(Err(format!("Failed to GET from '{}'", &url)))?;
-        let total_size = res
+    let total_size = res
         .content_length()
         .ok_or(format!("Failed to get content length from '{}'", &url))?;
-    
-        // Indicatif
-        let pb = ProgressBar::new(total_size);
-        pb.set_style(ProgressStyle::default_bar()
+
+    // Indicatif
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
         .template("{msg}\n[{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
         .progress_chars("#>-"));
-        pb.set_message(&format!("Downloading to {}", path));
-        
-        let mut file = File::create(path).or(Err(format!("Failed to create file '{}'", path)))?;
-        let mut downloaded: u64 = 0;
-        let mut stream = res.bytes_stream();
-        
-        while let Some(item) = stream.next().await {
-            let chunk = item.or(Err(format!("Error while downloading file")))?;
-            file.write_all(&chunk)
+    pb.set_message(&format!("Downloading to {}", path));
+
+    let mut file = File::create(path).or(Err(format!("Failed to create file '{}'", path)))?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err(format!("Error while downloading file")))?;
+        file.write_all(&chunk)
             .or(Err(format!("Error while writing to file")))?;
-            let new = min(downloaded + (chunk.len() as u64), total_size);
-            downloaded = new;
-            pb.set_position(new);
-        }
-        
-        pb.finish_with_message(&format!("Downloaded modpack to {}", path));
-        return Ok(());
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
     }
-    
+
+    pb.finish_with_message(&format!("Downloaded modpack to {}", path));
+    return Ok(());
+}
+
 pub fn end() {
     //i know this is stupid but its just to stop the code before exiting
     let mut end = String::new();
